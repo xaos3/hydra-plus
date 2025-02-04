17e55641604890a593b1cd37609afe9b5370559f
Deus Ex <nikosmourgis@gmail.com>
2025-02-05 00:12:20 +0200

    Add files via upload
    Add support for the -e flag that creates self excutable scripts

--- linux/hydra+/hydra.c
+++ linux/hydra+/hydra.c
@@ -108,164 +108,294 @@ int main(int argc, char** argv)
 	#endif
 /*************************/
 
-    char *main_script   = NULL  ;
-	bool params_exists  = false ;
+
+    char *main_script       = NULL  ;
+	PHDR_RAW_BUF embbed_scr = NULL  ;
+	bool params_exists      = false ;
 	/*check parameters and mode of operation*/
-	if( argc < 2 )
+
+	int is_exec = hdr_is_self_executed() ;/*check for self executable mode*/
+	if( is_exec == 1)
 	{
-	  printf("***Fatal Error : The minimum parameters that Hydra+ needs to execute a script is the full file name of the script\n");
-      printf("Examples :\n");
-	  printf("hydra+ /home/scripts/mainscript.hydra\n");
-	  printf("hydra+ \"C:\\Program Files\\scripts\\mainscript.hydra\" \n");
-	  return 0 ;
-	}
+      /*a self executed script! Load the internal embedded script */
+	  PHDR_RAW_BUF rawbuff = hdr_load_stored_script() ;
+	  if(rawbuff->len == 0)
+	  {
+		printf("***Fatal Error. The loading of the embedded script returned zero size.");
+	    return ;
+	  }
+	  embbed_scr = rawbuff ; 
 
+	  /*handle the parameters*/
+	  if(argc == 3 )
+	  {
+		  if((argv[1][0] == '-')&&(argv[1][1] == 'p'))
+		  {
+			if(argc < 3 ) 
+			{
+			   printf("***Fatal Error : After the -p flag a string of variable assigments for Hydra+ must be set. Example : -p [$var1=0,$var2=`hello`]\n");
+			   return ;
+			}
 
-	if(argv[1][0] != '-')
-	{
-	   /*retrieve next param as the scripts file name*/
-	   main_script = argv[1] ;
-	   /*check if command line sets any variables*/
-	   if(argc > 2 )
-	   {
-	     if((argv[2][0] == '-')&&(argv[2][1] == 'p'))
-		 {
-		   if(argc < 4 ) 
-		   {
-		    printf("***Fatal Error : After the -p flag a string of variable assigments for Hydra+ must be set. Example : -p [$var1=0,$var2=`hello`]\n");
-		   }
-
-		   /*set the flag to signal Hydra+ to load the parameters */
-		   params_exists = true ;
-		 }
-		 else
-		 {
-		  printf("***Fatal Error : The second parameter for the Hydra+ MUST be the -p flag\n");
-		  return 0 ;
-		 }
-	   
-	   }
+			params_exists = true ;
+		  }
+	  }
+	  else
+		  if(argc > 1)
+		  {
+			printf("***Fatal Error : In the self executable state only the -p [...] parameter is valid. \n");
+			return ;
+		  }
 
 	}
 	else
-	 if((argv[1][0] == '-')&&(argv[1][1] == 'o'))
-	 {
-		 /*load the file*/
-		 char * text = hdr_loader_load_text_from_disk(argv[2]) ;
-	     /*obfuscate the file*/
-		 PHDR_ENCRYPTED_BUF buf = hdr_loader_encrypt_buf_create(text) ;
-		 /*write the file in the disk*/
-		 PDX_STRING temps = dx_string_createU(NULL,argv[2]);
-		 PDX_STRING ext   = dx_string_createU(NULL,".obf") ;
-		 PDX_STRING newf  = dx_string_concat(temps,ext)	   ;
-		 enum  hdr_save_to_disk_state state = hdr_loader_save_binary_to_disk(newf->stringa, buf , true) ;
-		 switch(state)
-		 {
-			 case hdr_sd_filename_null : {printf("***Fatal Error : The file name for the obfuscated file is NULL\n");}break;
-			 case hdr_sd_ebuf_null	   : {printf("***Fatal Error : The [ebuf] is NULL\n");}break;
-			 case hdr_sd_ebuf_buf_null : {printf("***Fatal Error : The ebuffer is NULL\n");}break;
-			 case hdr_sd_no_open	   : {printf("***Fatal Error : Unable to open the file [%s]\n",newf->stringa);}break;
-			 case hdr_sd_success	   : {printf("Success! A new obfuscated file was created . File name : %s\n",newf->stringa);}break;
-		 }
-		 /*do not free any memory the program exits and its lifetime ends! :'( */
-		 return 0 ;
-	 }
-	 else
-	 if((argv[1][0] == '-')&&(argv[1][1] == 'c'))
-	 {
-
-		PHYDRA hydra = HydraCreate(argv[0], NULL); /*create the hydra object and set the executable's directory*/
-		if (hydra == NULL)
+		if(is_exec == -1)
 		{
-			printf("***FATAL ERROR : Hydra object does not created.\n");
-			return 0 ;
+		 printf("***Fatal Error. Error in is_exec -1\n");
+	     return ;
 		}
-		main_script = argv[2] ;
+	else
+	{
+		if( argc < 2 )
+		{
+		  printf("***Fatal Error : The minimum parameters that Hydra+ needs to execute a script is the full file name of the script\n");
+		  printf("Examples :\n");
+		  printf("hydra+ /home/scripts/mainscript.hydra\n");
+		  printf("hydra+ \"C:\\Program Files\\scripts\\mainscript.hydra\" \n");
+		  return 0 ;
+		}
+	
 
-		 /*setup the loader with the main script file*/
-		 if(HydraLoadScript(hydra , main_script) == false)
+		if(argv[1][0] != '-')
+		{
+		   /*retrieve next param as the script's file name*/
+		   main_script = argv[1] ;
+		   /*check if command line sets any variables*/
+		   if(argc > 2 )
+		   {
+			 if((argv[2][0] == '-')&&(argv[2][1] == 'p'))
+			 {
+			   if(argc < 4 ) 
+			   {
+				printf("***Fatal Error : After the -p flag a string of variable assigments for Hydra+ must be set. Example : -p [$var1=0,$var2=`hello`]\n");
+			   }
+
+			   /*set the flag to signal Hydra+ to load the parameters */
+			   params_exists = true ;
+			 }
+			 else
+			 {
+			  printf("***Fatal Error : The second parameter for the Hydra+ MUST be the -p flag\n");
+			  return 0 ;
+			 }
+	   
+		   }
+
+		}
+		else
+		 if((argv[1][0] == '-')&&(argv[1][1] == 'o'))
 		 {
-		   printf("***Fatal error. The operation failed.");
-		   return ;
+			 /*load the file*/
+			 char * text = hdr_loader_load_text_from_disk(argv[2]) ;
+			 /*obfuscate the file*/
+			 PHDR_ENCRYPTED_BUF buf = hdr_loader_encrypt_buf_create(text) ;
+			 /*write the file in the disk*/
+			 PDX_STRING temps = dx_string_createU(NULL,argv[2]);
+			 PDX_STRING ext   = dx_string_createU(NULL,".obf") ;
+			 PDX_STRING newf  = dx_string_concat(temps,ext)	   ;
+			 enum  hdr_save_to_disk_state state = hdr_loader_save_binary_to_disk(newf->stringa, buf , true) ;
+			 switch(state)
+			 {
+				 case hdr_sd_filename_null : {printf("***Fatal Error : The file name for the obfuscated file is NULL\n");}break;
+				 case hdr_sd_ebuf_null	   : {printf("***Fatal Error : The [ebuf] is NULL\n");}break;
+				 case hdr_sd_ebuf_buf_null : {printf("***Fatal Error : The ebuffer is NULL\n");}break;
+				 case hdr_sd_no_open	   : {printf("***Fatal Error : Unable to open the file [%s]\n",newf->stringa);}break;
+				 case hdr_sd_success	   : {printf("Success! A new obfuscated file was created . File name : %s\n",newf->stringa);}break;
+			 }
+			 /*do not free any memory the program exits and its lifetime ends! :'( */
+			 return 0 ;
 		 }
+		 else
+		 if((argv[1][0] == '-')&&(argv[1][1] == 'c'))
+		 {
+
+			PHYDRA hydra = HydraCreate(argv[0], NULL); /*create the hydra object and set the executable's directory*/
+			if (hydra == NULL)
+			{
+				printf("***FATAL ERROR : Hydra object did not created.\n");
+				return 0 ;
+			}
+			main_script = argv[2] ;
+
+			 /*setup the loader with the main script file*/
+			 if(HydraLoadScript(hydra , main_script,NULL) == false)
+			 {
+			   printf("***Fatal error. The operation failed.");
+			   return ;
+			 }
 		 
-		 PDX_STRINGLIST include_list = dx_stringlist_create() ;
-		 char *main_indx = NULL;
-		 if(hdr_compile_include_files(hydra->loader,include_list,&main_indx) == false)
-		 {
-		   printf("***Fatal error. The operation failed.");
-		   return ;
-		 }
+			 PDX_STRINGLIST include_list = dx_stringlist_create() ;
+			 char *main_indx = NULL;
+			 if(hdr_compile_include_files(hydra->loader,include_list,&main_indx) == false)
+			 {
+			   printf("***Fatal error. The operation failed.");
+			   return ;
+			 }
 
-		 /*now we will create the monolithic file*/
+			 /*now we will create the monolithic file*/
 
-		 PDX_STRING temps = dx_string_createU(NULL,argv[2]);
-		 PDX_STRING ext   = dx_string_createU(NULL,".comp") ;
-		 PDX_STRING newf  = dx_string_concat(temps,ext)	   ;
+			 PDX_STRING temps = dx_string_createU(NULL,argv[2]);
+			 PDX_STRING ext   = dx_string_createU(NULL,".comp") ;
+			 PDX_STRING newf  = dx_string_concat(temps,ext)	   ;
 
 		 
 
-		 FILE *f = fopen(newf->stringa,"wb") ;
+			 FILE *f = fopen(newf->stringa,"wb") ;
 
-		 PDXL_NODE node = include_list->start ;
-		 while(node!=NULL)
-		 {
-		   PDX_STRING file_name = node->object->key ; 
-           FILE *inf = fopen(file_name->stringa,"rb");
-		   DXLONG64 fsize = dx_GetFileSize(inf) ;
-		   char * buff = (char*)malloc(fsize)   ;
-		   fread(buff,fsize,1,inf);
-		   fwrite(buff,fsize,1,f) ;
-		   node = node->right ;
-		 }
+			 PDXL_NODE node = include_list->start ;
+			 while(node!=NULL)
+			 {
+			   PDX_STRING file_name = node->object->key ; 
+			   FILE *inf = fopen(file_name->stringa,"rb");
+			   DXLONG64 fsize = dx_GetFileSize(inf) ;
+			   char * buff = (char*)malloc(fsize)   ;
+			   fread(buff,fsize,1,inf);
+			   fwrite(buff,fsize,1,f) ;
+			   node = node->right ;
+			 }
 
-		 /*write the monolithic file*/
-		 fwrite(main_indx,strlen(main_indx),1,f) ;
+			 /*write the monolithic file*/
+			 fwrite(main_indx,strlen(main_indx),1,f) ;
 
-		 fclose(f);
+			 fclose(f);
 
-		 PDX_STRING monolithic = dx_string_createU(NULL,newf->stringa) ;
-		 newf = dx_string_free(newf);
+			 PDX_STRING monolithic = dx_string_createU(NULL,newf->stringa) ;
+			 newf = dx_string_free(newf);
 		
-		 /*load the file*/
-		 char * text = hdr_loader_load_text_from_disk(monolithic->stringa) ;
+			 /*load the file*/
+			 char * text = hdr_loader_load_text_from_disk(monolithic->stringa) ;
 
-		 PHDR_ENCRYPTED_BUF buf = hdr_loader_encrypt_buf_create(text) ;
-		 /*write the file in the disk*/
-		 temps = dx_string_createU(NULL,argv[2]);
-		 ext   = dx_string_createU(NULL,".obf") ;
-		 newf  = dx_string_concat(temps,ext)	   ;
-		 enum  hdr_save_to_disk_state state = hdr_loader_save_binary_to_disk(newf->stringa, buf , true) ;
-		 switch(state)
-		 {
-			 case hdr_sd_filename_null : {printf("***Fatal Error : The file name for the obfuscated file is NULL\n");}break;
-			 case hdr_sd_ebuf_null	   : {printf("***Fatal Error : The [ebuf] is NULL\n");}break;
-			 case hdr_sd_ebuf_buf_null : {printf("***Fatal Error : The ebuffer is NULL\n");}break;
-			 case hdr_sd_no_open	   : {printf("***Fatal Error : Unable to open the file [%s]\n",newf->stringa);}break;
-			 case hdr_sd_success	   : {printf("Success! A new monolithic obfuscated file was created . File name : %s\n",newf->stringa);}break;
+			 PHDR_ENCRYPTED_BUF buf = hdr_loader_encrypt_buf_create(text) ;
+			 /*write the file in the disk*/
+			 temps = dx_string_createU(NULL,argv[2]);
+			 ext   = dx_string_createU(NULL,".obf") ;
+			 newf  = dx_string_concat(temps,ext)	   ;
+			 enum  hdr_save_to_disk_state state = hdr_loader_save_binary_to_disk(newf->stringa, buf , true) ;
+			 switch(state)
+			 {
+				 case hdr_sd_filename_null : {printf("***Fatal Error : The file name for the obfuscated file is NULL\n");}break;
+				 case hdr_sd_ebuf_null	   : {printf("***Fatal Error : The [ebuf] is NULL\n");}break;
+				 case hdr_sd_ebuf_buf_null : {printf("***Fatal Error : The ebuffer is NULL\n");}break;
+				 case hdr_sd_no_open	   : {printf("***Fatal Error : Unable to open the file [%s]\n",newf->stringa);}break;
+				 case hdr_sd_success	   : {printf("Success! A new monolithic obfuscated file was created . File name : %s\n",newf->stringa);}break;
+			 }
+			 /*do not free any memory the program exits and its lifetime ends! :'( */
+			 return 0 ;
 		 }
-		 /*do not free any memory the program exits and its lifetime ends! :'( */
-		 return 0 ;
+		 else
+		 if((argv[1][0] == '-')&&(argv[1][1] == 'e'))
+		 {
+
+			PHYDRA hydra = HydraCreate(argv[0], NULL); /*create the hydra object and set the executable's directory*/
+			if (hydra == NULL)
+			{
+				printf("***FATAL ERROR : Hydra object did not created.\n");
+				return 0 ;
+			}
+			main_script = argv[2] ;
+
+			 /*setup the loader with the main script file*/
+			 if(HydraLoadScript(hydra , main_script,NULL) == false)
+			 {
+			   printf("***Fatal error. The operation failed.");
+			   return ;
+			 }
+		 
+			 PDX_STRINGLIST include_list = dx_stringlist_create() ;
+			 char *main_indx = NULL;
+			 if(hdr_compile_include_files(hydra->loader,include_list,&main_indx) == false)
+			 {
+			   printf("***Fatal error. The operation failed.");
+			   return ;
+			 }
+
+			 /*now we will create the monolithic file*/
+
+			 PDX_STRING temps = dx_string_createU(NULL,argv[2]);
+			 PDX_STRING ext   = dx_string_createU(NULL,".comp") ;
+			 PDX_STRING newf  = dx_string_concat(temps,ext)	   ;
+
+		 
+
+			 FILE *f = fopen(newf->stringa,"wb") ;
+
+			 PDXL_NODE node = include_list->start ;
+			 while(node!=NULL)
+			 {
+			   PDX_STRING file_name = node->object->key ; 
+			   FILE *inf = fopen(file_name->stringa,"rb");
+			   DXLONG64 fsize = dx_GetFileSize(inf) ;
+			   char * buff = (char*)malloc(fsize)   ;
+			   fread(buff,fsize,1,inf);
+			   fwrite(buff,fsize,1,f) ;
+			   node = node->right ;
+			 }
+
+			 /*write the monolithic file*/
+			 fwrite(main_indx,strlen(main_indx),1,f) ;
+
+			 fclose(f);
+
+			 PDX_STRING monolithic = dx_string_createU(NULL,newf->stringa) ;
+			 newf = dx_string_free(newf);
+		
+			 /*load the file*/
+			 char * text = hdr_loader_load_text_from_disk(monolithic->stringa) ;
+
+			 PHDR_ENCRYPTED_BUF buf = hdr_loader_encrypt_buf_create(text) ;
+			 /*write the file in the disk*/
+			 temps = dx_string_createU(NULL,argv[2]);
+			 ext   = dx_string_createU(NULL,".obf") ;
+			 newf  = dx_string_concat(temps,ext)	   ;
+			 enum  hdr_save_to_disk_state state = hdr_loader_save_binary_to_disk(newf->stringa, buf , true) ;
+			 switch(state)
+			 {
+				 case hdr_sd_filename_null : {printf("***Fatal Error : The file name for the obfuscated file is NULL\n");}break;
+				 case hdr_sd_ebuf_null	   : {printf("***Fatal Error : The [ebuf] is NULL\n");}break;
+				 case hdr_sd_ebuf_buf_null : {printf("***Fatal Error : The ebuffer is NULL\n");}break;
+				 case hdr_sd_no_open	   : {printf("***Fatal Error : Unable to open the file [%s]\n",newf->stringa);}break;
+				 case hdr_sd_success	   : {printf("Success! A new executable script file was created.\n");}break;
+			 }
+
+			 /*now we load the obfuscated compiled file and create an executable*/
+			 hdr_create_executable(newf->stringa);
+
+			 /*do not free any memory the program exits and its lifetime ends! :'( */
+			 return 0 ;
+		 }
+		 else
+		 {
+		  printf("***Fatal Error : The flag [%s] is not a valid flag for the Hydra+ command line. valid flags : [-o,-c,-e]\n",argv[1]);
+		  return 0 ;
+		 }	
 	 }
-	 else
-	 {
-	  printf("***Fatal Error : The flag [%s] is not a valid flag for the Hydra+ command line. valid flags : [-o,-c]\n",argv[1]);
-	  return 0 ;
-	 }	
 
 	PHYDRA hydra = HydraCreate(argv[0], NULL); /*create the hydra object and set the executable's directory*/
 	if (hydra == NULL)
 	{
-		printf("***FATAL ERROR : Hydra object does not created.\n");
+		printf("***FATAL ERROR : Hydra object did not created.\n");
 		return 0 ;
 	}
 
 	if(params_exists == true)
 	{
-	  if(HydraLoadParams(hydra,argv[4]) == false) return 0 ; /*the function has print the appropriate error code*/
+	  char *param_l = argv[4] ;
+	  /*in the self executable state the params are the third string */
+	  if(embbed_scr != NULL) param_l = argv[2] ; 
+	  if(HydraLoadParams(hydra,param_l) == false) return 0 ; /*the function has print the appropriate error code*/
 	}
 
-	if (HydraLoadScript(hydra, main_script) == false)
+	if (HydraLoadScript(hydra, main_script,embbed_scr) == false)
 	{
 		printf("***Fatal Error : The script was not loaded. Script : %s\n",argv[2]);
 	}
